8f4c019b5f1d0acab43bf3127691b5b31d8c4123
chamny20 <dge3179@ajou.ac.kr>
2025-06-15 22:54:16 +0900

    chaemin/ch5: add answer code about ex3

--- chaemin/chapter-5/ex3.ts
+++ chaemin/chapter-5/ex3.ts
@@ -4,3 +4,46 @@
  * 기존에는 항상 Shoe가 반환되었지만 이번에는 사용자가 Shoe.create('boot')를 호출하면 Boot를 반환하고,
  * Shoe.create('balletFlat')를 호출하면 BalletFlat을 반환할 것임을 컴파일 타임에 알 수 있도록 바꿔보자.
  */
+
+type Shoe = {
+  purpose: string;
+};
+
+class BalletFlat implements Shoe {
+  purpose = "dancing";
+}
+
+class Boot implements Shoe {
+  purpose = "woodcutting";
+}
+
+class Sneaker implements Shoe {
+  purpose = "walking";
+}
+
+// 오버로드된 시그니처
+interface ShoeFactory {
+  create(type: "balletFlat"): BalletFlat;
+  create(type: "boot"): Boot;
+  create(type: "sneaker"): Sneaker;
+}
+
+// 신발 팩토리
+const Shoe: ShoeFactory = {
+  create(type: "balletFlat" | "boot" | "sneaker") {
+    switch (type) {
+      case "balletFlat":
+        return new BalletFlat();
+      case "boot":
+        return new Boot();
+      case "sneaker":
+        return new Sneaker();
+    }
+  },
+};
+
+const shoe1 = Shoe.create("boot"); // 타입은 Boot
+const shoe2 = Shoe.create("sneaker"); // 타입은 Sneaker
+const shoe3 = Shoe.create("balletFlat"); // 타입은 BalletFlat
+
+console.log(shoe1.purpose); // 'woodcutting'
